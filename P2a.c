104ddd465d0083054288cfe01c37248f779494ad
Tippala Girish <tippalagirish61@gmail.com>
2025-11-18 15:43:24 +0530

    Add files via upload

---
+++ P2a.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10;              
+    float b = 3.14;          
+    double c = 123.456789;
+    char d = 'A';            
+    char name[20] = "vsaishamanth"; 
+
+    printf("Integer value: %d\n", a);
+    printf("Float value: %.2f\n", b);
+    printf("Double value: %.6lf\n", c);
+    printf("Character value: %c\n", d);
+    printf("String value: %s\n", name);
+
+    return 0;
+}
